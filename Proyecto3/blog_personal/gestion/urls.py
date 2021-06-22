589dcf756d27cc44c87ae58fe71e9135fad07785
Ricardo William <billysoftperu@gmail.com>
2021-06-21 22:17:24 -0500

    validacion de usuarios,prestamos,libros

---
+++ Proyecto3/blog_personal/gestion/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (LibrosController,
+                    LibroController,
+                    busqueda_libros,
+                    buscador_edicion,
+                    UsuariosController,
+                    PrestamosController)
+
+urlpatterns = [
+    path('libros', LibrosController.as_view(), name='create-read-libros'),
+    path('libros/<int:id>', LibroController.as_view()),
+    path('busqueda_libros', busqueda_libros),
+    path('busqueda_libros_edicion', buscador_edicion),
+    path('usuarios', UsuariosController.as_view()),
+    path('prestamos', PrestamosController.as_view()),
+]
