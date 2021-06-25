18aab069c268ebb00c255a5eb347576e7c678e2e
Ricardo William <billysoftperu@gmail.com>
2021-06-24 22:18:55 -0500

    subiendo imagenes

--- Proyecto4/cms/views.py
+++ Proyecto4/cms/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .models import PlatoModel
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.generics import (
+    CreateAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView)
+from .serializers import *
 
-# Create your views here.
+
+class ArchivosController(CreateAPIView):
+    serializer_class = ArchivoSerializer
+
+    def post(self, request: Request):
+
+        data = self.serializer_class(data=request.FILES)
+        data.is_valid(raise_exception=True)
+        data.save()
+        return Response('ok')
+
+
+class PlatosController(ListCreateAPIView):
+    queryset = PlatoModel.objects.all()
+    serializer_class = PlatoSerializer
+
+    def post(self, request: Request):
+        # para ver que archivos me esta mandando el frontend
+        print(request.FILES)
+        return Response(data='ok')
